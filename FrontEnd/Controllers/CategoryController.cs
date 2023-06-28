89e91e96528da687f84ea9f6f78bdf246861f691
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-06-28 18:22:00 +0300

    kategori silme işlemi tamamlandı

--- FrontEnd/Controllers/CategoryController.cs
+++ FrontEnd/Controllers/CategoryController.cs
@@ -5,34 +5,45 @@ using System.Text.Json;
 
 namespace FrontEnd.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
+        private readonly HttpClient client;
         public CategoryController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
+            client = _httpClientFactory.CreateClient();
         }
 
-        public async  Task<IActionResult> List()
+        public async Task<IActionResult> List()
         {
-            var token=User.Claims.FirstOrDefault(x=>x.Type=="accessToken")!.Value;
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")!.Value;
             if (token != null)
             {
-                var client = _httpClientFactory.CreateClient();
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",token);
-              var response=  await  client.GetAsync("http://localhost:5125/api/categories");
-                if (response.IsSuccessStatusCode) 
+
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync("http://localhost:5125/api/categories");
+                if (response.IsSuccessStatusCode)
                 {
-                  var jsonData = await response.Content.ReadAsStringAsync();
-                  var result=  JsonSerializer.Deserialize<List<CategoryListModel>> (jsonData, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<List<CategoryListModel>>(jsonData, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
                     return View(result);
                 }
-              
+
             }
-            
+
             return View();
+        }
+        public async Task<IActionResult> Remove(int id)
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")!.Value;
+            if (token != null)
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await client.DeleteAsync($"http://localhost:5125/api/categories/{id}");
+            }
+            return RedirectToAction("List");
         }
     }
 }
