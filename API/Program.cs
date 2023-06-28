a8e047512de163c440a119de994f0942f6141fe6
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-06-28 14:45:50 +0300

    bug fixed

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+});
 builder.Services.AddDbContext<CoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("Local"));
