2dfbc2f6e7292a0f2fe5ec2d76dcf528b0ca539a
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-06-30 15:23:17 +0300

    product complete

--- FrontEnd/Controllers/ProductController.cs
+++ FrontEnd/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using FrontEnd.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text;
 using System.Text.Json;
 
 namespace FrontEnd.Controllers
@@ -23,7 +25,7 @@ namespace FrontEnd.Controllers
             if (token != null)
             {
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                var response = await client.GetAsync("http://localhost:5125/api/product");
+                var response = await client.GetAsync("http://localhost:5125/api/products");
                 if (response.IsSuccessStatusCode)
                 {
                     var jsonData = await response.Content.ReadAsStringAsync();
@@ -43,5 +45,60 @@ namespace FrontEnd.Controllers
             }
             return RedirectToAction("List");
         }
+
+        public async Task <IActionResult> Create()
+        {
+            var model = new ProductCreateModel();
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")!.Value;
+            if (token != null)
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync($"http://localhost:5125/api/categories");
+                if (response.IsSuccessStatusCode) 
+                {
+                    var jsonData= await response.Content.ReadAsStringAsync();
+                 var data=   JsonSerializer.Deserialize<List<CategoryListModel>>(jsonData, new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                    model.Categories=new Microsoft.AspNetCore.Mvc.Rendering.SelectList(data,"Id", "Definition");
+                    return View(model);
+                }
+              
+            }
+             
+            return RedirectToAction("List");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateModel model)
+        {
+            var data=TempData["Categories"]?.ToString();
+            if (data!=null)
+            {
+                var categories = JsonSerializer.Deserialize<List<SelectListItem>>(data);
+                model.Categories=new SelectList(categories,"Value","Text");
+            }
+            if (ModelState.IsValid)
+            {
+                var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")!.Value;
+                if (token != null)
+                {
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                    var jsonData = JsonSerializer.Serialize(model);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                    var response = await client.PostAsync("http://localhost:5125/api/products", content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("List");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Bir hata meydana geldi");
+                    }
+                }
+            }
+            return View(model);
+        }
+
     }
 }
---
+++ FrontEnd/Models/ProductCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models
+{
+    public class ProductCreateModel
+    {
+
+        [Required(ErrorMessage = "Ürün Adı Zorunludur")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Stok Zorunludur")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "Fiyat Zorunludur")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Kategori Adı Zorunludur")]
+        public int CategoryId { get; set; }
+        public SelectList? Categories { get; set; }
+    }
+}
